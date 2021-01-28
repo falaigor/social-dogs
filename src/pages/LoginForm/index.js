870b1d2280deb8dc05c4ece301d450eb2ff89c21
Igor Santos <falaigors@gmail.com>
2021-01-27 23:20:35 -0300

    Project Finish

--- src/pages/LoginForm/index.js
+++ src/pages/LoginForm/index.js
@@ -38,7 +38,7 @@ const LoginForm = () => {
         ) : (
           <Button>Entrar</Button>
         )}
-        <Error error={error} />
+        <Error error={error && 'Dados incorretos'} />
       </form>
       <Link className={styles.reset} to="/login/lost">
         Perdeu a senha?
